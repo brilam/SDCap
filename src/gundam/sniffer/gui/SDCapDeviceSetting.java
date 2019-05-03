8a6e461d80be5e33ad1b552ebd388ce37c3e66b3
brilam <brian_lam@live.com>
2019-05-03 17:05:21 -0400

    Finished the GUI and listener for device settings

--- src/gundam/sniffer/gui/SDCapDeviceSetting.java
+++ src/gundam/sniffer/gui/SDCapDeviceSetting.java
@@ -7,13 +7,26 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import org.pcap4j.core.PcapNetworkInterface;
+import gundam.sniffer.config.SniffingConfiguration;
 import pcap4j.Pcap4jWrapper;
 
 public class SDCapDeviceSetting {
   private JFrame frame;
+  private JComboBox<String> devices;
+  private JTextField portTextField;
+  private static final String DEFAULT_PORT = "5001"; 
+  private static final int UNSET_PORT_VAL = -1;
+  private static final int MIN_PORT_VAL = 0;
+  private static final int MAX_PORT_VAL = 65535;
+  private static final String PORT_ERROR_MESSAGE =
+      String.format("Port must be a number between %d and %d!", MIN_PORT_VAL, MAX_PORT_VAL);
+  private SniffingConfiguration sc;
+
 
   /**
    * Launch the application.
@@ -69,7 +82,7 @@ public class SDCapDeviceSetting {
     frame.getContentPane().add(networkDeviceLabel);
 
     // Add the network devices to the JComboBox
-    JComboBox<String> devices = new JComboBox<>(Pcap4jWrapper.getDeviceNames());
+    devices = new JComboBox<>(Pcap4jWrapper.getDeviceNames());
     devices.setBounds(11, 34, 394, 20);
     frame.getContentPane().add(devices);
 
@@ -79,17 +92,43 @@ public class SDCapDeviceSetting {
     frame.getContentPane().add(portLabel);
 
     // Adds the port text field
-    JTextField portTextField = new JTextField();
+    portTextField = new JTextField();
     portTextField.setBounds(11, 77, 86, 20);
+    portTextField.setText(DEFAULT_PORT);
     frame.getContentPane().add(portTextField);
     portTextField.setColumns(10);
 
     // Adds OK button to confirm the sniffing configuration
     JButton okButton = new JButton("OK");
-    okButton.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent arg0) {}
-    });
+    okButton.addActionListener(new DeviceSettingListener());
     okButton.setBounds(325, 96, 80, 23);
     frame.getContentPane().add(okButton);
+  }
+  
+  private class DeviceSettingListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent event) {
+      int deviceIndex = devices.getSelectedIndex();
+      int portNumber = UNSET_PORT_VAL;
+      
+      try {
+        portNumber = Integer.parseInt(portTextField.getText());
+      } catch (NumberFormatException e) {
+        JOptionPane.showMessageDialog(frame, PORT_ERROR_MESSAGE, "Invalid port value!", 
+            JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      
+      // If the port isn't within the valid port range
+      if (!(portNumber >= MIN_PORT_VAL && portNumber <= MAX_PORT_VAL)) {
+        JOptionPane.showMessageDialog(frame, PORT_ERROR_MESSAGE, "Invalid port value!", 
+            JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      
+      PcapNetworkInterface device = Pcap4jWrapper.getAllDevices().get(deviceIndex);
+      sc = new SniffingConfiguration.SniffingConfigurationBuilder().setSniffingDevice(device)
+          .setPort(portNumber).build();
+    }
   }
 }
